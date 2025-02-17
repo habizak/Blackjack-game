97fc2e4f34bdf82f0a818a525e63e2531e787c01
habizak <antrix00@gmail.com>
2025-02-17 12:38:37 +0800

    Added more JS

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-let firstCard = 10
-let secondCard = 11
+let firstCard = 5
+let cardOne = document.querySelector("#card-1")
+let secondCard = 3
+let cardTwo = document.querySelector("#card-2")
+let sumVal = document.querySelector("#sum-val")
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.querySelector("#messageEl")
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+function newGame() {
+    
 }
 
-console.log(message)
+function renderGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    cardOne.textContent = `${firstCard}`;
+    cardTwo.textContent = `${secondCard}`;
+    sumVal.textContent = `${sum}`;
+    messageEl.textContent = `"${message}"`;
+}
+
+function drawCard() {
+    let newCard = 5
+    sum += newCard
+    renderGame()
+}
